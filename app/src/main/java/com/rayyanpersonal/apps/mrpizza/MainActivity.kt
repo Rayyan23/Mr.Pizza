4dbf3574a8ea02764a9b029c6e15377c64f1aa51
Rayyan <Ray23fast@gmail.com>
2022-07-02 14:30:47 +0300

    first activity fixed, needs functionality

--- app/src/main/java/com/rayyanpersonal/apps/mrpizza/MainActivity.kt
+++ app/src/main/java/com/rayyanpersonal/apps/mrpizza/MainActivity.kt
@@ -2,17 +2,32 @@ package com.rayyanpersonal.apps.mrpizza
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    val username: EditText = findViewById<EditText>(R.id.user)
-    val pass: EditText= findViewById<EditText>(R.id.pass)
+    private lateinit var username: EditText
+    private lateinit var pass:EditText
+    private lateinit var login:Button
+
+    private lateinit var signup: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SetViews()
+        val toast = Toast.makeText(applicationContext, "Hello Javatpoint", Toast.LENGTH_LONG)
+        toast.show()
 
+    }
 
+    private fun SetViews() {
+        username = findViewById<EditText>(R.id.user)
+        pass= findViewById<EditText>(R.id.pass)
+        login= findViewById<Button>(R.id.login)
+        signup= findViewById<Button>(R.id.signup)
     }
 }
